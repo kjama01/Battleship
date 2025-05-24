fe2df8a0beb3cc00b6c511ef7f50f675c94b3756
Donquellso <kamil080801@gmail.com>
2025-05-24 03:29:54 +0200

    fix: prevent invalid ship creation and add Gameboard test Added Jest test for ship placement, hits, misses, and sinking Fixed column case in ship placement logic Prevent creating ships outside the allowed pool

--- gameboard.js
+++ gameboard.js
@@ -2,13 +2,21 @@ import Ship from "./ship.js";
 
 export default class Gameboard {
   #ships = [];
+  #availableShips = [5, 4, 3, 3, 2];
   #gameboard;
+  #miss = 0;
   constructor() {
     this.#gameboard = Array.from({ length: 10 }, () => Array(10).fill(null));
   }
   placeShip(x, y, length) {
+    const index = this.#availableShips.indexOf(length);
+    if (index === -1) {
+      return;
+    }
+    this.#availableShips.splice(index, 1);
     const ship = new Ship(length);
     let count = 0;
+    if (y + length > 10) return false;
     for (let i = 0; i < length; i++) {
       if (this.#gameboard[x][y + i]) {
         for (let j = 0; j < count; j++) {
@@ -21,5 +29,28 @@ export default class Gameboard {
     }
     this.#ships.push(ship);
     return ship;
+  }
+  receiveAttack(x, y) {
+    if (!this.#gameboard[x][y]) {
+      this.#miss++;
+      return (this.#gameboard[x][y] = "MISS");
+    }
+    const ship = this.#gameboard[x][y];
+    this.#gameboard[x][y] = "HIT";
+    ship.hit();
+    if (ship.isSunk()) {
+      this.#ships.splice(this.#ships.indexOf(ship), 1);
+      return "SUNK";
+    }
+    return "HIT";
+  }
+  getMisses() {
+    return this.#miss;
+  }
+  isAllSunk() {
+    return this.#ships.length === 0;
+  }
+  displayBoard() {
+    return this.#gameboard.map((row) => [...row]);
   }
 }
